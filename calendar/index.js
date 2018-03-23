355f732166ea993d7ccb60e12789f12130110877
Matei Copot <matei@copot.eu>
2018-03-23 18:56:06 +0100

    changed msg-bar format to `title_ dd~hh`

--- calendar/index.js
+++ calendar/index.js
@@ -26,7 +26,7 @@ title = title.length > opts.eventNameCharAmount ?
     const hour = date.getHours().toString().padStart(2, '0');
     const minute = date.getMinutes().toString().padStart(2, '0');
 
-    date = `${day}<${hour}`;
+    date = `${day}~${hour}`;
 
 
     return `${date} ${title}`;
