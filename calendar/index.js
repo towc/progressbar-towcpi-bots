b6f369db8c4d9963e76847507b8871907e0f0bcb
Matei Copot <matei@copot.eu>
2018-03-24 08:26:21 +0100

    changed format to `dd<hh title[-]`

--- calendar/index.js
+++ calendar/index.js
@@ -17,16 +17,16 @@ const opts = {
 calendarRSS.listen(({ items }) => {
   items = items.slice(0, msgBar.height);
   const msg = items.map(({ title, date }) => {
-title = title.length > opts.eventNameCharAmount ?
-      title.substring(0, opts.eventNameCharAmount - 1) + '_' :
-      title.padEnd(' ', opts.eventNameCharAmount);
+    title = title.length > opts.eventNameCharAmount
+      ? `${title.substring(0, opts.eventNameCharAmount)}-`
+      : `${title.padEnd(' ', opts.eventNameCharAmount)}`;
 
     const day = date.getDate().toString().padStart(2, '0');
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const hour = date.getHours().toString().padStart(2, '0');
     const minute = date.getMinutes().toString().padStart(2, '0');
 
-    date = `${day}~${hour}`;
+    date = `${day}<${hour}`;
 
 
     return `${date} ${title}`;
